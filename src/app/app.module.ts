9e2f9bd35686f5a1128a59994afc626774810c85
Ritu <rituwale22@gmail.com>
2023-03-15 19:24:21 +0530

    home page imp wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from "./core/core.module";
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
