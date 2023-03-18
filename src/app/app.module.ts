ee77dfeefa3d5d8d431cb53c0881b5467caacba7
Ritu <rituwale22@gmail.com>
2023-03-18 23:13:59 +0530

    top-deals implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from "./core/core.module";
 import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import  { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
+
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        MedicineHomeComponent,
+        TopDealsComponent
+         
+        
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        BrowserAnimationsModule ,
+        CarouselModule,
+        HttpClientModule,
+        FormsModule ,
+        
+        
+        
+
     ]
 })
 export class AppModule { }
